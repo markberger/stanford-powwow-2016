f31a9ba62c6696a86253aab33b2c7a08ec45903e
Mark J. Berger <mark.berger.j@gmail.com>
2015-04-13 21:12:25 -0700

    Init commit with fabfile and requirements

---
+++ fabfile.py
@@ -0,0 +1,27 @@
+from fabric.api import local
+from fabric.api import put
+from fabric.api import task
+from fabric.api import execute
+from flask_frozen import Freezer
+from main import app
+
+HOST = "corn.stanford.edu"
+
+@task
+def freeze():
+	print("Building static version of app...")
+	freezer = Freezer(app)
+	freezer.freeze()
+	print("Freezing complete.")
+
+def pushToAFS():
+        put("build/*", "/afs/ir.stanford.edu/group/powwow/WWW")
+
+@task
+def deploy():
+	print "Preparing deployment to Stanford AFS..."
+	freeze()
+	print "Uploading files to AFS..."
+	user = raw_input("SUNetID: ")
+	execute(pushToAFS, hosts=[user+'@'+HOST])
+
